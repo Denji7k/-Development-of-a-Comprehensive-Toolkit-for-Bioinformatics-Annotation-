1a664212ac6f299b44d1eb08c9155bb2a878adc0
Denji7k <arun04vyas@gmail.com>
2024-07-03 13:01:36 +0530

    Update dna_motif.py

--- dna_motif.py
+++ dna_motif.py
@@ -14,15 +14,17 @@ def parse_fasta(file_path):
 def find_motif_positions(sequence, motif_sequence):
     return [i + 1 for i in range(len(sequence) - len(motif_sequence) + 1) if sequence[i:i+len(motif_sequence)] == motif_sequence]
 
-input_file_path = input("Enter the input file: ").strip()
-output_file_path = input("Enter the output file (leave blank to use input file name): ").strip() or f"{input_file_path}_output.txt"
+input_file = input("Enter the input file: ").strip()
+output_file = input("Enter the output file (leave blank to use input file name): ").strip() or f"{input_file}_output.txt"
+
 motif_sequence = input("Enter the motif sequence to search for: ").strip()
 
-sequences = parse_fasta(input_file_path)
+sequences = parse_fasta(input_file)
+
 sequence_positions = {seq_id: find_motif_positions(seq, motif_sequence) for seq_id, seq in sequences.items()}
 
-with open(output_file_path, 'w') as output_file:
+with open(output_file, 'w') as output_file:
     for seq_id, positions in sequence_positions.items():
         output_file.write(f"{seq_id}: {', '.join(map(str, positions))}\n")
 
-print(f"Positions of '{motif_sequence}' in each sequence have been written to {output_file_path}")
+print(f"Positions of '{motif_sequence}' in each sequence have been written to {output_file}")
