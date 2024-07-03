c3cfa43537f6ba743a8e2c97151d238a7856d3a9
Denji7k <arun04vyas@gmail.com>
2024-07-03 12:36:59 +0530

    Update cvs.py

--- cvs.py
+++ cvs.py
@@ -1,23 +1,18 @@
 def read_csv(file_name):
-    """Reads CSV data, skipping the header, and returns a list of (position, residue) tuples."""
     with open(file_name, 'r') as f:
-        next(f)  # Skip header
+        next(f)
         return [(int(line.split(',')[0]), line.split(',')[1].strip()) for line in f]
 
-# Get input file names
 input_file1 = input("Enter the first CSV file name: ").strip()
 input_file2 = input("Enter the second CSV file name: ").strip()
 
-# Read sequences
 seq1 = read_csv(input_file1)
 seq2 = read_csv(input_file2)
 
-# Output file names
 out1_file = 'out_1.txt'
 out2_file1 = f'out_2_{input_file1}'
 out2_file2 = f'out_2_{input_file2}'
 
-# Write results
 with open(out1_file, 'w') as out1, \
      open(out2_file1, 'w') as out2_1, \
      open(out2_file2, 'w') as out2_2:
