f820576e24638e5b6cf5134b9f7fdeceb6014e78
Denji7k <arun04vyas@gmail.com>
2024-07-03 12:30:23 +0530

    Update Mendel's First Law.py

--- Mendel's First Law.py
+++ Mendel's First Law.py
@@ -4,11 +4,9 @@ def firstLaw(k, m, n):
     N = (k + m + n)
     return (1 - (1 / N) * ((n * (n - 1) + n * m + m * (m - 1) / 4) / (N - 1)))
 
-# Read k,m,n values from file
 with open("organism.txt", 'r') as file:
     k, m, n = map(int, file.readline().split())
 
-# Calculate result
 result = firstLaw(k, m, n)
 
 with open("output.txt", 'w') as output_file:
