8737807273020352d2ae00720bbdb417808df0b6
Denji7k <arun04vyas@gmail.com>
2024-07-03 16:36:50 +0530

    Update len_seq.py

--- len_seq.py
+++ len_seq.py
@@ -3,30 +3,37 @@ from collections import OrderedDict
 def parse_fasta(file_path):
     sequences = OrderedDict()
     current_key = None
+
     with open(file_path, 'r') as file:
         for line in file:
             line = line.strip()
             if line.startswith('>'):
+                if current_key is not None:
+                    sequences[current_key] = ''.join(sequences.get(current_key, ''))
                 current_key = line[1:]
                 sequences[current_key] = ''
             else:
-                sequences[current_key] += line
+                if current_key is not None:
+                    sequences[current_key] += line
+
+        if current_key is not None:
+            sequences[current_key] = ''.join(sequences.get(current_key, ''))
+
     return sequences
 
 def calculate_sequence_length(sequence):
     return len(sequence)
 
 # Get input and output file paths from the user
-input_file_path = input("Enter the input file: ").strip()
-output_file_path = input("Enter the output file (leave blank to use input file name): ").strip() or f"{input_file_path}_output.txt"
+input_file = input("Enter the input file: ").strip()
+output_file = input("Enter the output file (leave blank to use input file name): ").strip() or f"{input_file}_output.txt"
 
-
-parsed_fa = parse_fasta(input_file_path)
+parsed_fa = parse_fasta(input_file)
 
 # Write result for each sequence into a new file
-with open(output_file_path, 'w') as output_file:
+with open(output_file, 'w') as file:
     for key, value in parsed_fa.items():
         seq_length = calculate_sequence_length(value)
-        output_file.write(f"{key}: The length of the sequence is: {seq_length}\n")
+        file.write(f"{key}: The length of the sequence is: {seq_length}\n")
 
-print(f"The length of each sequence has been written to {output_file_path}")
+print(f"The length of each sequence has been written to {output_file}")
