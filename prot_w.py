63091986e1671fdcce6dcd0e1d462fdc81fb1d2f
Denji7k <arun04vyas@gmail.com>
2024-07-03 15:33:58 +0530

    Update prot_w.py

--- prot_w.py
+++ prot_w.py
@@ -23,21 +23,19 @@ protein_mass_table = {
     "T": 101.04768, "V": 99.06841, "W": 186.07931, "Y": 163.06333
 }
 
-# input FASTA file path
-input_file_path = input("Enter the input file: ").strip()
-output_file_path = input("Enter the output file (leave blank to use input file name): ").strip() or f"{input_file_path}_output.txt"
-
+# Input and output file paths
+input_file = input("Enter the input file: ").strip()
+output_file = input("Enter the output file (leave blank to use input file name with '_output.txt' suffix): ").strip() or f"{input_file}_output.txt"
 
 # Parse the sequences from the input FASTA file
-sequences = parse_fasta(input_file_path)
+sequences = parse_fasta(input_file)
 
 # Calculate total masses for each sequence
 sequence_masses = {seq_id: calculate_total_mass(seq.upper(), protein_mass_table) for seq_id, seq in sequences.items()}
 
 # Write total masses to the output file
-with open(output_file_path, 'w') as output_file:
+with open(output_file, 'w') as file:
     for seq_id, mass in sequence_masses.items():
-        output_file.write(f"{seq_id}: {mass}\n")
+        file.write(f"{seq_id}: {mass}\n")
 
-print(f"Total masses of protein sequences have been written to {output_file_path}")
-
+print(f"Total masses of protein sequences have been written to {output_file}")
