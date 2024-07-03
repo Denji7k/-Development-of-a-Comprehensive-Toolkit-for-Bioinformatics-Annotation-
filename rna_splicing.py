ab1dcb34a79ca3b01d193ab148c9fbdfc47f8329
Denji7k <arun04vyas@gmail.com>
2024-07-03 16:04:47 +0530

    Update rna_splicing.py

--- rna_splicing.py
+++ rna_splicing.py
@@ -1,30 +1,4 @@
-def read_fasta(file_path):
-    sequences = {}
-    key = None
-    with open(file_path, 'r') as file:
-        for line in file:
-            line = line.strip()
-            if line.startswith('>'):
-                key = line[1:]
-                sequences[key] = ''
-            elif key:
-                sequences[key] += line
-    return sequences
-
-
-def parse_fasta(fasta_data):
-    sequences = {}
-    for key, value in fasta_data.iteritems():
-        sequences[key] = value.strip()
-    return sequences
-
-def transcribe_translate(dna_sequence, introns):
-    for intron in introns:
-        dna_sequence = dna_sequence.replace(intron, '')
-
-    rna_sequence = dna_sequence.replace('T', 'U')
-
-    protein_sequence = ''
+def translate_rna_to_protein(rna_sequence):
     codon_table = {
         "UUU": "F", "CUU": "L", "AUU": "I", "GUU": "V",
         "UUC": "F", "CUC": "L", "AUC": "I", "GUC": "V",
@@ -41,50 +15,41 @@ def transcribe_translate(dna_sequence, introns):
         "UGU": "C", "CGU": "R", "AGU": "S", "GGU": "G",
         "UGC": "C", "CGC": "R", "AGC": "S", "GGC": "G",
         "UGA": "STOP", "CGA": "R", "AGA": "R", "GGA": "G",
-        "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G" }
+        "UGG": "W", "CGG": "R", "AGG": "R", "GGG": "G"
+    }
 
+    protein_sequence = ''
     for i in range(0, len(rna_sequence), 3):
         codon = rna_sequence[i:i+3]
         amino_acid = codon_table.get(codon, '')
         if amino_acid == "STOP":
-            break  # Stop translating at stop codon
+            break
         protein_sequence += amino_acid
 
     return protein_sequence
 
-def get_protein_sequences(fasta_data):
-    parsed_fa = parse_fasta(fasta_data)
-    dna_sequences = list(parsed_fa.values())
-    introns = dna_sequences[1:]
+# Prompt user for input and output file paths
+input_file = input("Enter the path to the input FASTA file: ")
+output_file = input("Enter the path to the output file: ") or f"{input_file}_output.txt"
 
-    protein_sequences = {}
-    for key, dna_sequence in parsed_fa.items():
-        protein_sequences[key] = transcribe_translate(dna_sequence, introns)
+# Read the FASTA file
+sequences = {}
+with open(input_file, 'r') as file:
+    key = None
+    for line in file:
+        line = line.strip()
+        if line.startswith('>'):
+            key = line[1:]
+            sequences[key] = ''
+        elif key:
+            sequences[key] += line
 
-    return protein_sequences
+# Translate RNA to protein
+protein_sequences = {key: translate_rna_to_protein(seq) for key, seq in sequences.items()}
 
-file_path = 'fasta_sequence.txt'
-dna_sequences = read_fasta(file_path)
-protein_sequences = get_protein_sequences(dna_sequences)
+# Write the protein sequences to the output file
+with open(output_file, 'w') as file:
+    for key, protein_sequence in protein_sequences.items():
+        file.write(f'{key}: {protein_sequence}\n')
 
-for key, protein_sequence in protein_sequences.iteritems():
-    print('{0}: {1}'.format(key, protein_sequence))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(f"Protein sequences have been written to {output_file}.")
