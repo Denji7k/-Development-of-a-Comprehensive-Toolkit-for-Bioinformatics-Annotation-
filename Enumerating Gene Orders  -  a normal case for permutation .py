a67fbe8a18aa15b7a679b42bd18f802bc2cb44ca
Denji7k <arun04vyas@gmail.com>
2024-07-31 10:59:47 +0530

    Update Enumerating Gene Orders  -  a normal case for permutation .py

--- Enumerating Gene Orders  -  a normal case for permutation .py
+++ Enumerating Gene Orders  -  a normal case for permutation .py
@@ -1,11 +1,12 @@
 import math
 import itertools
 
-n = 2
+n = int(input('Eneter the value:'))
 factorial_n = math.factorial(n)
 permutations = itertools.permutations(range(1, n + 1))
 
-with open('output.txt', 'w') as output_file:
+output_file = input("Enter the output file (leave blank to use input file name): ").strip() or f"{"gene order"}_output.txt"
+with open(output_file, 'w') as output_file:
     output_file.write(f"{factorial_n}\n")
     for perm in permutations:
         output_file.write(' '.join(map(str, perm)) + "\n")
